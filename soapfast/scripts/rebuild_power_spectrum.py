2f120d64f356682674d46237de43992c5eedde7f
alanmlewis <alanlewis372@gmail.com>
2023-06-01 14:27:55 +0200

    Correctly rebuild _natoms files

--- soapfast/scripts/rebuild_power_spectrum.py
+++ soapfast/scripts/rebuild_power_spectrum.py
@@ -28,10 +28,12 @@ blocksize = int(math.ceil(float(ndata)/float(nblocks)))
 
 # Put power spectra of individual blocks together into a single power spectrum
 pslices = []
-natom_slices = []
+#natom_slices = []
+natoms = []
 for i in range(nblocks):
     pslices.append(np.load(fname + "_" + str(i) + ".npy"))
-    natom_slices.append(np.load(fname + "_" + str(i) + "_natoms.npy"))
+    natoms += list(np.load(fname + "_" + str(i) + "_natoms.npy"))
+#    natom_slices.append(np.load(fname + "_" + str(i) + "_natoms.npy"))
     if args.cleanup:
         remove(fname + "_" + str(i) + ".npy")
         remove(fname + "_" + str(i) + "_natoms.npy")
@@ -39,7 +41,8 @@ for i in range(nblocks):
 	
 
 power = np.vstack(pslices)
-natoms = np.vstack(natom_slices)
+#natoms = np.vstack(natom_slices)
+natoms = np.array(natoms)
 
 np.save(fname + ".npy",power)
 np.save(fname + "_natoms.npy",natoms)
